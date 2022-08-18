fe991230e8a06d48d89bb9336b872cd23ba656d2
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-08-18 13:51:36 +0300

    created question struct

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Nikita  on 8/18/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question{
+    let questionText: String
+    var questionAnswer: String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    
+    @IBOutlet var trueButton: UIButton!
+    @IBOutlet var falseButton: UIButton!
+    
+    let quiz = [Question(questionText: "Four + Two is equal to Six. ", questionAnswer: "True"), Question(questionText: "Five - Three is greater than One", questionAnswer: "True"), Question(questionText: "Three + Eight is less than Ten", questionAnswer: "False")]
+    var questionNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnser = quiz[questionNumber].questionAnswer
+        
+        
+        if userAnswer == actualAnser{
+            print("Right!")
+        }
+        else {
+            print("Wrong!")
+        }
+        
+        if quiz.count - 1 != questionNumber{
+            questionNumber += 1
+            updateUI()
+        }
+        else {
+            questionNumber = 0
+            updateUI()
+        }
+      
+    }
+    
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber].questionText
+        
+    }
+    
 
 
 }
+
+
+
 
